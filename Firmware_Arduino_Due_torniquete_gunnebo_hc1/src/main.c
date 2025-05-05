c7c8a1e90cb0ba9c21164341796864fafeae5b4b
The_heri24 <jose.heriberto@hotmail.es>
2025-05-05 01:06:17 -0500

    indicacion de los pulso del encoder con led en modo polling
    mejorando la legibilidad del codigo y portabilidad.
    posiblemente mejor con manejo de struct  en c.
    
    Presente: sensar senal de encoder  y verlo directamente en 2 led en modo polling
    Futuro: hacer lo mismo pero con manejo de  interrupciones.

--- Firmware_Arduino_Due_torniquete_gunnebo_hc1/src/main.c
+++ Firmware_Arduino_Due_torniquete_gunnebo_hc1/src/main.c
@@ -5,9 +5,9 @@
 *	-hay 3 sensores de  efecto hall disponible, dos para paso preciso y otro para indicar el paso del torniquete en si.
 *	-2 solenoide con entrada a 24 v.
 *
-* Presente: inicializando el proyecto, empezando haciendo un blink con driver PIO.
+* Presente: sensar senal de encoder  y verlo directamente en 2 led en modo polling
 *
-* Futuro: hacer polling con dos pines de salida led junto con las senales de los 2 encoder.
+* Futuro: hacer lo mismo pero con manejo de  interrupciones.
 *
 * Notas:
 * Arduino Due Digital Pin Mapping (D0–D13)
@@ -32,6 +32,24 @@
 * +--------------+----------------+	
 */
 
+// salidas led
+#define PIN_D3 PIO_PC28_IDX
+#define PIN_D3_MASK PIO_PC28
+#define PIN_D3_PORT PIOC
+
+#define PIN_D2 PIO_PB25_IDX
+#define PIN_D2_MASK PIO_PB25
+#define PIN_D2_PORT PIOB
+
+// entrada encoder
+#define PIN_D4 PIO_PC26_IDX
+#define PIN_D4_MASK PIO_PC26
+#define PIN_D4_PORT PIOC
+
+#define PIN_D5 PIO_PC25_IDX
+#define PIN_D5_MASK PIO_PC25
+#define PIN_D5_PORT PIOC
+
 #include <asf.h>
 
 void configure_pins(void);
@@ -40,7 +58,16 @@ void configure_pins(void)
 {
 	// Habilitar el reloj para el PIOB
 	pmc_enable_periph_clk(ID_PIOB);	
-	pio_configure(PIOB,PIO_OUTPUT_1,PIO_PB27,PIO_DEFAULT);
+	pmc_enable_periph_clk(ID_PIOC);	
+	
+	// salida
+	pio_configure(PIN_D2_PORT,PIO_OUTPUT_0,PIN_D2_MASK,PIO_DEFAULT);
+	pio_configure(PIN_D3_PORT,PIO_OUTPUT_0,PIN_D3_MASK,PIO_DEFAULT);
+	
+	// entrada
+	pio_configure(PIN_D4_PORT,PIO_INPUT,PIN_D4_MASK,PIO_DEFAULT);
+	pio_configure(PIN_D5_PORT,PIO_INPUT,PIN_D5_MASK,PIO_DEFAULT);
+	
 }
 
 
@@ -49,9 +76,31 @@ int main (void)
 	sysclk_init();
 	board_init();
 	configure_pins();
+	
+	uint8_t encoder_1=0;
+	uint8_t encoder_2=0;
 	while (1) 
 	{
-		pio_toggle_pin(PIO_PB27_IDX);
-		delay_ms(500);
+		encoder_1 = pio_get(PIN_D5_PORT,PIO_INPUT,PIN_D5_MASK);
+		encoder_2 = pio_get(PIN_D4_PORT,PIO_INPUT,PIN_D4_MASK);
+
+		if (encoder_1)
+		{
+			pio_set(PIN_D3_PORT,PIN_D3_MASK);
+		} 
+		else
+		{
+			pio_clear(PIN_D3_PORT,PIN_D3_MASK);		
+		}
+		
+		if (encoder_2)
+		{
+			pio_set(PIN_D2_PORT,PIN_D2_MASK);
+		}
+		else
+		{
+			pio_clear(PIN_D2_PORT,PIN_D2_MASK);
+		}
+		delay_ms(10);
 	}
 }
