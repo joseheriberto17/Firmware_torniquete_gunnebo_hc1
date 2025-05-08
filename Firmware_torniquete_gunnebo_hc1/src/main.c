037c417a7d151816311d736e52f0743c228193df
LabNebulae <soporte.equipos@nebulae.com.co>
2025-05-07 19:20:16 -0500

    .....

--- Firmware_torniquete_gunnebo_hc1/src/main.c
+++ Firmware_torniquete_gunnebo_hc1/src/main.c
@@ -26,6 +26,14 @@
 #define PIC_1_PIN_MASK PIO_PA8
 #define PIC_1_PIN_PORT PIOA
 
+#define PIC_2_PIN PIO_PA8_IDX
+#define PIC_2_PIN_MASK PIO_PA8
+#define PIC_2_PIN_PORT PIOA
+
+// parametros de la aplicacion
+#define MAX_REVERSE_TOLERANCE 12 // define cuantos contadores puede contar si el torniquete se devuelve
+#define DIR_CLOCKWISE true // sentido de giro
+
 const int8_t qdec_table[4][4] = {
 	// to:  00   01   10   11
 	/*from 00*/ {  0, -1, +1,  0 },
@@ -37,10 +45,17 @@ const int8_t qdec_table[4][4] = {
 
 
 // variables globales
-volatile int32_t position = 0;
-volatile uint8_t last_state = 0;
-volatile bool new_dir = true;
-volatile bool error_dir = true;
+volatile int32_t position_encoder = 0;
+volatile int32_t position_encoder_last = 0;
+volatile int32_t position_encoder_reverse = 0;
+
+volatile uint8_t last_state_encoder = 0;
+volatile bool dir_encoder_new = true;
+volatile int8_t delta_last = 0;
+volatile bool end_pase = true;
+volatile bool clockwise = true;
+volatile bool change_dir_detected = false;
+
 
 
 // prototipos de funciones
@@ -57,30 +72,128 @@ uint8_t read_AB(void) {
 // Handler llamado por fase A o fase B, encoder tipo X4
 void handle_encoder(const uint32_t id, const uint32_t mask) {
 	// lectura de los encoder
-	 uint8_t new_state = read_AB();
-	 uint8_t End_pase = pio_get(INDEX_PIN_PORT, PIO_INPUT, INDEX_PIN_MASK) ? 1 : 0;
-	 
-	 // validar contador 
-	 int8_t delta = qdec_table[last_state][new_state];
-	 if (delta != 0) {
-		 position += delta; // posicion
-		 new_dir = (delta > 0); // direccion
-	 }
-	 
-	 
-	 if (new_dir)
-	 {
-		 pio_set(SEN_I_PIN_PORT,SEN_I_PIN_MASK);
-		 pio_clear(SEN_S_PIN_PORT,SEN_S_PIN_MASK);
-	 }
-	 else
-	 {
-		 pio_clear(SEN_I_PIN_PORT,SEN_I_PIN_MASK);
-		 pio_set(SEN_S_PIN_PORT,SEN_S_PIN_MASK);
-	 }
-
-	 last_state = new_state;
+	uint8_t new_state_encoder = read_AB();
+	end_pase = pio_get(INDEX_PIN_PORT, PIO_INPUT, INDEX_PIN_MASK) ? 1 : 0;
+	
+	
+	int8_t delta = qdec_table[last_state_encoder][new_state_encoder];// validar el conteo de encoder
+	if (delta != 0)
+	{
+		position_encoder += delta;
+		dir_encoder_new = (delta > 0);
+		
+		if (end_pase) // Estas en final de carrera
+		{
+			//validar paso completo
+			
+			if ((change_dir_detected == false) && (abs(position_encoder_last) > 60) )
+			{
+				pio_toggle_pin(PIC_1_PIN);
+				position_encoder = 0; // reinicia el contador de posicion
+			}
+			
+			position_encoder_last= 0; // reinicias
+			change_dir_detected = false; // ignora los cambio de direccion
+			delta_last = delta; // almacenas el ultimo valor
+			
+		}
+		else
+		{
+			
+			if (abs(position_encoder_last) < abs(position_encoder))
+			{
+				position_encoder_last = position_encoder;
+			}
+			
+			if (abs(position_encoder_last - position_encoder) > MAX_REVERSE_TOLERANCE)
+			{
+				position_encoder = 0;
+				change_dir_detected = true;
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			//if (clockwise == !(delta > 0)) // mientra gire en en el sentido correcto
+			//{
+			//// si vas en el sentido de giro definido , cuenta posicion y direccion
+			//dir_encoder_new = (delta > 0);
+			//}
+			//
+			//if (clockwise)
+			//{
+			//
+			//if (position_encoder_last > position_encoder)
+			//{
+			//position_encoder_last = position_encoder;
+			//}
+			//
+			//}
+			//else
+			//{
+			//if (position_encoder_last < position_encoder)
+			//{
+			//position_encoder_last = position_encoder;
+			//}
+			//}
+			//
+			//if (abs(position_encoder_last - position_encoder) > MAX_REVERSE_TOLERANCE)
+			//{
+			//position_encoder = 0;
+			//change_dir_detected = true;
+			//}
+			//
+			
+			
+			//if (clockwise == !(delta > 0)) // mientra gire en en el sentido correcto
+			//{
+			//// si vas en el sentido de giro definido , cuenta posicion y direccion
+			//position_encoder += delta;
+			//new_dir_encoder = (delta > 0);
+			//position_encoder_reverse = 0;
+			//
+			//}
+			//else
+			//{
+			//position_encoder_reverse++;
+			//if (position_encoder_reverse > MAX_REVERSE_TOLERANCE)
+			//{
+			//position_encoder = 0;   // anular valor de posicion
+			//change_dir_detected = true; // activar el detecto devolucion invalida
+			//}
+			//}
+		}
+	}
+	
+	
+	if (change_dir_detected)
+	{
+		pio_set(SEN_I_PIN_PORT, SEN_I_PIN_MASK);
+		pio_set(SEN_S_PIN_PORT, SEN_S_PIN_MASK);
+	}
+	else if (dir_encoder_new)
+	{
+		pio_set(SEN_I_PIN_PORT, SEN_I_PIN_MASK);
+		pio_clear(SEN_S_PIN_PORT, SEN_S_PIN_MASK);
+	}
+	else
+	{
+		pio_clear(SEN_I_PIN_PORT, SEN_I_PIN_MASK);
+		pio_set(SEN_S_PIN_PORT, SEN_S_PIN_MASK);
+	}
+	
+	last_state_encoder = new_state_encoder;
 }
+
+
 
 
 
@@ -97,10 +210,10 @@ void configure_pins(void) {
 	
 	// Entrada GPIO
 	pio_configure(PHASE_A_PIN_PORT,PIO_INPUT,PHASE_A_PIN_MASK,PIO_DEFAULT);
-	pio_configure(PHASE_B_PIN_PORT,PIO_INPUT,PHASE_B_PIN_MASK,PIO_DEFAULT);	
+	pio_configure(PHASE_B_PIN_PORT,PIO_INPUT,PHASE_B_PIN_MASK,PIO_DEFAULT);
 	pio_configure(INDEX_PIN_PORT,PIO_INPUT,INDEX_PIN_MASK,PIO_DEFAULT);
 	pio_configure_interrupt(PHASE_A_PIN_PORT,PHASE_A_PIN_MASK, PIO_IT_EDGE);
-	pio_configure_interrupt(PHASE_B_PIN_PORT,PHASE_B_PIN_MASK, PIO_IT_EDGE);	
+	pio_configure_interrupt(PHASE_B_PIN_PORT,PHASE_B_PIN_MASK, PIO_IT_EDGE);
 	pio_handler_set(PIOB, ID_PIOB, PHASE_A_PIN_MASK|PHASE_B_PIN_MASK, PIO_IT_EDGE, handle_encoder);
 	
 	// Habilitar la interrupción en el periférico y en el NVIC
@@ -114,10 +227,12 @@ int main(void) {
 	// deshabilitar el watchdog
 	WDT->WDT_MR = WDT_MR_WDDIS;
 	board_init();
-	configure_pins();	
-	last_state = read_AB();
+	configure_pins();
 	
-	while (1){
+	last_state_encoder = read_AB();
+	
+	while (1)
+	{
 		
 		
 	}
