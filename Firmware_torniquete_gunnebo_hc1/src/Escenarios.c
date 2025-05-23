8ad9e688cd262b667ac30fdd2773d52d63e34605
@soporte46 <soporte@nebulae.com.co>
2025-05-23 17:08:43 -0500

    organizar el codigo

--- Firmware_torniquete_gunnebo_hc1/src/Escenarios.c
+++ Firmware_torniquete_gunnebo_hc1/src/Escenarios.c
@@ -7,6 +7,7 @@
 #include <asf.h>
 #include "Escenarios.h"
 
+
 // Pin de Entrada SW2_1
 #define SW2_1_PIN PIO_PA23_IDX
 #define SW2_1_PIN_MASK PIO_PA23
@@ -29,12 +30,31 @@
 
 
 
- uint8_t read_ABCD(void)
+ void configuracion_escenario(void)
 {
+	// lectura de SW2_1
 	uint8_t A = pio_get(SW2_1_PIN_PORT, PIO_INPUT, SW2_1_PIN_MASK) ? 1 : 0;
 	uint8_t B = pio_get(SW2_2_PIN_PORT, PIO_INPUT, SW2_2_PIN_MASK) ? 1 : 0;
 	uint8_t C = pio_get(SW2_3_PIN_PORT, PIO_INPUT, SW2_3_PIN_MASK) ? 1 : 0;
 	uint8_t D = pio_get(SW2_4_PIN_PORT, PIO_INPUT, SW2_4_PIN_MASK) ? 1 : 0;
-	return (A << 3) | (B << 2) | (C << 1) | D;
+
+	// return (A << 3) | (B << 2) | (C << 1) | D;
+	if (A)
+	{
+		pio_set()
+	}
+	else
+	{
+		/* code */
+	}
+	
+	typedef struct {
+		char Name[10];
+		uint8_t Addr;
+		uint8_t Value;      
+		bool Update; 
+	} registers;
+	
+
 }
 
